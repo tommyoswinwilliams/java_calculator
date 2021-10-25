52566e66d80e7719cf08d616df0a624eb5792c06
Tommy Oswin Williams <tommy@teow.co.uk>
2021-10-25 16:50:34 +0100

    Gradle and test and method for add two ints method

---
+++ app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+  public static void main(String[] args) {
+  }
+
+  public Integer add(Integer a, Integer b) {
+    Integer result = a + b;
+    return result;
+  }
+}
